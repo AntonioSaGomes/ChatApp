044262082789db1aed25fadfd5bf8c8e83b7bbe8
Antonio Gomes <showzen97@gmail.com>
2023-01-12 18:39:58 +0000

    css change for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import SideMenu from "./components/SideMenu/SideMenu";
 import MainContainer from "./components/MainContainer/MainContainer";
